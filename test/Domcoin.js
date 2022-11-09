071e468fd4350c5d9a88d8c8c356c4338a8e32dd
domininik <dominik.fijas+github@gmail.com>
2022-11-09 22:14:03 +0100

    Test mint, burn and basic interface

--- test/Domcoin.js
+++ test/Domcoin.js
@@ -25,4 +25,49 @@ describe("Domcoin", function () {
       expect(await token.owner()).to.equal(owner.address);
     });
   });
+
+  describe("Interface", function () {
+    it("Returns token name", async function () {
+      const { token } = await loadFixture(deployFixture);
+
+      expect(await token.name()).to.equal("Domcoin");
+    });
+
+    it("Returns token symbol", async function () {
+      const { token } = await loadFixture(deployFixture);
+
+      expect(await token.symbol()).to.equal("DOM");
+    });
+
+    it("Returns total supply", async function () {
+      const { token } = await loadFixture(deployFixture);
+
+      expect(await token.totalSupply()).to.equal(1000);
+    });
+  });
+
+  describe("Mint", function () {
+    it("Increases total supply", async function () {
+      const { token, otherAccount } = await loadFixture(deployFixture);
+      await token.mint(otherAccount.address, 500);
+
+      expect(await token.totalSupply()).to.equal(1500);
+    });
+
+    it("Transfers minted tokens to given account", async function () {
+      const { token, otherAccount } = await loadFixture(deployFixture);
+      await token.mint(otherAccount.address, 500);
+
+      expect(await token.balanceOf(otherAccount.address)).to.equal(500);
+    });
+  });
+
+  describe("Burn", function () {
+    it("Decreases total supply", async function () {
+      const { token } = await loadFixture(deployFixture);
+      await token.burn(50);
+
+      expect(await token.totalSupply()).to.equal(950);
+    });
+  });
 });
