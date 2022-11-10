25ffaa222dc06b87d9ea9fcd96c34f4c22fcd277
domininik <dominik.fijas+github@gmail.com>
2022-11-10 20:32:36 +0100

    Use ethers to access coin contract

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,49 @@
+import React from "react";
+import { ethers } from "ethers";
+import Domcoin from "./artifacts/contracts/Domcoin.sol/Domcoin.json";
 
-function App() {
-  return (
-    <div className="App">
-      Yo
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    name: ''
+  }
+
+  componentDidMount() {
+    if (typeof window.ethereum !== 'undefined') {
+      this.initialize();
+    } else {
+      console.log("ERROR: web3 provider not available");
+    };
+  };
+
+  async initialize() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    let contract;
+
+    try {
+      const signerAddress = await signer.getAddress();
+      const contractAddress = ethers.utils.getContractAddress({
+        from: signerAddress,
+        nonce: 0
+      });
+      contract = new ethers.Contract(contractAddress, Domcoin.abi, provider);
+    } catch (error) {
+      console.log(error);
+    }
+
+    const name = await contract.name();
+
+    this.setState({ name: name });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p>Name: {this.state.name }</p>
+      </div>
+    );
+  }
 }
 
 export default App;
